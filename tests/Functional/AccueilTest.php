186e7bf9fb5485a6d4c5e72e24f961b5f12b0e64
Abdessalam ZENNOU <107582950+ZaiimZinu@users.noreply.github.com>
2026-03-26 20:42:31 +0100

    test updated

--- tests/Functional/AccueilTest.php
+++ tests/Functional/AccueilTest.php
@@ -6,11 +6,65 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class AccueilTest extends WebTestCase
 {
-    public function testAccueil(): void
+    public function testAccueilPageLoads(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertResponseStatusCodeSame(200);
+    }
+
+    public function testAccueilPageUsesCorrectTemplate(): void
     {
         $client = static::createClient();
         $client->request('GET', '/');
 
         $this->assertResponseIsSuccessful();
+        $this->assertSelectorExists('html');
+    }
+
+    public function testAccueilDisplaysFormations(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $this->assertResponseIsSuccessful();
+    }
+
+    public function testCguPageLoads(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/cgu');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertResponseStatusCodeSame(200);
+    }
+
+    public function testCguPageUsesCorrectTemplate(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/cgu');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorExists('html');
+    }
+
+    public function testAccueilReturnsHtmlContent(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertResponseHeaderSame('Content-Type', 'text/html; charset=UTF-8');
+    }
+
+    public function testCguReturnsHtmlContent(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/cgu');
+
+        $this->assertResponseIsSuccessful();
+        $this->assertResponseHeaderSame('Content-Type', 'text/html; charset=UTF-8');
     }
 }
